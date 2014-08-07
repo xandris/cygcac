d11d63236acdc4d96ae8da6cbfe8a44e9bbef556
Dustin Parker <dparker@forwardslope.com>
2014-08-06 17:30:54 -0700

    Better apt-cyg usage, add README.md

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-wget 'https://apt-cyg.googlecode.com/svn/trunk/apt-cyg'
-chmod +x apt-cyg
-./apt-cyg install gcc-g++ make autoconf automake vim zip unzip git libtool patch pkg-config openssl openssl-devel curl libiconv-devel
+if ! [ -x /usr/local/bin/apt-cyg ]; then
+    curl 'https://apt-cyg.googlecode.com/svn/trunk/apt-cyg' > /usr/local/bin/apt-cyg
+    chmod +x /usr/local/bin/apt-cyg
+fi
+
+apt-cyg install gcc-g++ make autoconf automake vim zip unzip git libtool patch pkg-config openssl openssl-devel curl libiconv-devel
+
 make
